abce5668fdd2dccc122a2bbc40567bb45c879522
fernando822 <84929752+fernando822@users.noreply.github.com>
2021-07-23 22:28:53 -0300

    corrigiendo error

--- Assets/Scripts/LogicaMovimientoVehiculo.cs
+++ Assets/Scripts/LogicaMovimientoVehiculo.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class LogicaMovimientoVehiculo : MonoBehaviour
 {
     float speed = 1;
-    float direction = 1;
+    float directionSpeed = 1;
 
 
     public void PlayerDemolitionRace_Movement(Vector2 value)
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed * value.x);
 
-        transform.eulerAngles += new Vector3(0, value.y * Time.deltaTime, 0);
+        transform.eulerAngles += new Vector3(0, value.y * Time.deltaTime * directionSpeed, 0);
 
     }
     public void PlayerDemolitionRace_HandBrake()
