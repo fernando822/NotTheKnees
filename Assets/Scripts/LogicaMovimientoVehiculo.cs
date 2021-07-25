3ecc24f07f160fea809b38b08e111bc690fa8c95
Maicol Freire <lolmaicol@hotmail.com>
2021-07-24 23:30:46 -0300

    Arreglado el movimiento del vehiculo

--- Assets/Scripts/LogicaMovimientoVehiculo.cs
+++ Assets/Scripts/LogicaMovimientoVehiculo.cs
@@ -4,46 +4,63 @@ using UnityEngine;
 
 public class LogicaMovimientoVehiculo : MonoBehaviour
 {
-    
-    float directionSpeed = 1;
-    float aceleration =1;
-    float speedLimit = 10;
+    float directionSpeed = 30f;
+    float aceleration = 30f;
+    float speedLimit = 70f;
+    float speed = 0f;
+    float direccionInputHorizontal;
+    float direccionInputVertical;
+    float drag = 1f;
 
-    public void PlayerDemolitionRace_Movement(Vector2 value)
+    private void Start()
     {
-        float speed =+ Time.deltaTime * aceleration * value.x;
-        speed = Mathf.Clamp(speed,-speedLimit * 0.1f, speedLimit);
+    }
 
-        transform.Translate(Vector3.forward * speed);
+    public void PlayerDemolitionRaceMovement()
+    {
+        speed = Mathf.Clamp(speed, -speedLimit * 0.4f, speedLimit);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
 
+    void SetSpeed()
+    {
+        
+        if (direccionInputVertical != 0)
+        {
+            speed += Time.deltaTime * aceleration * direccionInputVertical;
+        }
+        else
+        {
+            speed = Mathf.Lerp(speed, 0, drag * Time.deltaTime);
+        }
+    }
 
-        if(speed != 0)
-        transform.eulerAngles += new Vector3(0, value.y * Time.deltaTime * directionSpeed, 0);
+    void Rotate()
+    {
+        float anguloDeRotacion = direccionInputHorizontal * Time.deltaTime * directionSpeed * direccionInputVertical;
+        transform.Rotate(Vector3.up, anguloDeRotacion);
+        
+    }
+    public void PlayerDemolitionRaceHandBrake()
+    {
 
     }
-    public void PlayerDemolitionRace_HandBrake()
-    {
 
+    public void PlayerDemolitionRaceTurbo()
+    {
+        
 
 
 
     }
-    public void PlayerDemolitionRace_Turbo()
+
+    public void FixedUpdate()
     {
-
-
-
-
-    }
-    public void Update()
-    {
-
-
-
-
-
-
-
+        direccionInputVertical = Input.GetAxis("Vertical");
+        direccionInputHorizontal = Input.GetAxis("Horizontal");
+        SetSpeed();
+        PlayerDemolitionRaceMovement();
+        Rotate();
     }
 
 }
