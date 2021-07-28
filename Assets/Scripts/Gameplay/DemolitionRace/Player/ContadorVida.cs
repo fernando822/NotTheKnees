588c8bc14f945095dd3a9e60840b6f59609aa1cb
fernando822 <84929752+fernando822@users.noreply.github.com>
2021-07-27 23:05:10 -0300

    implementando barra de vida

--- Assets/Scripts/Gameplay/DemolitionRace/Player/ContadorVida.cs
+++ Assets/Scripts/Gameplay/DemolitionRace/Player/ContadorVida.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ContadorVida : MonoBehaviour
 {
     public int vida;
     public int VidaACambiar;
+    public Image BarraDeVida;
 
     void Start()
     {
      vida = 100;
     }
 
-    void CambiarVida (int VidaACambiar)
+    public void CambiarVida (int VidaACambiar)
     {
         vida += VidaACambiar;
 
         if (vida <= 0)
             Destroy(gameObject);
+
+        vida = Mathf.Clamp(vida, 0, 100);
+
+        BarraDeVida.fillAmount = vida * 0.01f ;
     }
+    private void Update()
+    {
+        CambiarVida(0);
+    }
+
+
+
 }
