869cf2549298be064db2c3c3eccb09545dcc704e
Maicol Freire <lolmaicol40@gmail.com>
2021-08-17 12:05:12 -0300

    Modificado script movimiento vehiculo

--- Assets/Scripts/Gameplay/DemolitionRace/Player/LogicaMovimientoVehiculo.cs
+++ Assets/Scripts/Gameplay/DemolitionRace/Player/LogicaMovimientoVehiculo.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class LogicaMovimientoVehiculo : MonoBehaviour
 {
     [SerializeField] DemolitionRacePlayer player;
-    [SerializeField] float directionSpeed = 30f;
+    [SerializeField] float directionSpeed = 0.1f;
     [SerializeField] float aceleration = 30f;
     [SerializeField] float speedLimit = 70f;
-    [SerializeField] float drag = 1f; 
-   
-    float speed = 0f;
+    [SerializeField] float drag = 1f;
+
+    float speed;
+
+    public animaciones animaciones;
+
 
     private void Start()
     {
@@ -21,10 +24,14 @@ public class LogicaMovimientoVehiculo : MonoBehaviour
     {
         speed = Mathf.Clamp(speed, -speedLimit * 0.4f, speedLimit);
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        animaciones.road(speed);
     }
 
     public void SetSpeedAndRotation(float direccionInputHorizontal, float direccionInputVertical)
     {
+        animaciones.direccion(direccionInputHorizontal);
+        
+
         if (direccionInputVertical != 0)
         {
             speed += Time.deltaTime * aceleration * direccionInputVertical;
@@ -34,8 +41,8 @@ public class LogicaMovimientoVehiculo : MonoBehaviour
             speed = Mathf.Lerp(speed, 0, drag * Time.deltaTime);
         }
 
-        float anguloDeRotacion = direccionInputHorizontal * Time.deltaTime * directionSpeed * direccionInputVertical;
-        transform.Rotate(Vector3.up, anguloDeRotacion);
+        float anguloDeRotacion = direccionInputHorizontal * Time.deltaTime * directionSpeed * speed;
+        transform.Rotate(Vector3.right, anguloDeRotacion);
         
     }
     public void PlayerDemolitionRaceHandBrake()
@@ -56,7 +63,4 @@ public class LogicaMovimientoVehiculo : MonoBehaviour
         PlayerDemolitionRaceMovement();    
     }
 
-    public float GetSpeed() => speed;
-
-    public void SetSpeed(float speed) => this.speed = speed;
 }
