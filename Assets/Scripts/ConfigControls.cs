217e4707d528ca8880e182cf3ce45ace6b3e8512
fernando822 <84929752+fernando822@users.noreply.github.com>
2021-10-23 18:11:23 -0300

    sigiendo con los input

--- Assets/Scripts/ConfigControls.cs
+++ Assets/Scripts/ConfigControls.cs
@@ -2,31 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 
 public class ConfigControls : MonoBehaviour
 {
     private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
+
     [SerializeField] InputAction inputAction;
+    [SerializeField] InputActionReference inputActionReference;
+    [SerializeField] private GameObject button;
+    
 
+    private string actionName;
+    private int selectedBinding;
+    [SerializeField] int bindingIndex;
+    private InputBinding inputBinding;
 
-
-    private void A()
+    private void Start()
     {
-        rebindingOperation = inputAction.PerformInteractiveRebinding()
+       
+    }
+    private void Rebinding()
+    {
+
+        Debug.Log("fgszdfgdf");
+        button.SetActive(false);
+
+        rebindingOperation = inputActionReference.action.PerformInteractiveRebinding(bindingIndex)
             .WithControlsExcluding("<Mouse>/position")
             .WithControlsExcluding("<Mouse>/delta")
             .WithControlsExcluding("<Keyboard>/escape")
             .WithControlsExcluding("<Gamepad>/Start")
             .OnMatchWaitForAnother(0.2f)
             .OnComplete(rebindingOperation => b());
-
+     
         rebindingOperation.Start();
     }
 
     private void b()
     {
         rebindingOperation.Dispose();
+        button.SetActive(true);
     }
 
 }
